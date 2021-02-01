6d2aed3ced0909cb1859b539be723e81474dd246
CrownKira <t.karwi@yahoo.com>
2021-02-01 18:24:06 +0800

    Access api key via process.env

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-
-const KEY = 'AIzaSyARP6uiez1FTBfTck7uWcfupOQ1yxG6w_c';
+const KEY = process.env.GOOGLE_API_KEY;
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
